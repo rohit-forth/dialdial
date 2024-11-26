1ef917e0c78baaba27664d28d5990ac5663c2eff
Rohit <rohit.henceforth@gmail.com>
2024-11-26 17:56:47 +0530

    remove sign in

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -1,6 +1,5 @@
 
 import { Metadata } from 'next';
-import SignInViewPage from '../_components/sigin-view';
 
 export const metadata: Metadata = {
   title: 'Authentication | Sign In',
@@ -8,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <SignInViewPage />;
+  return <></>;
 }
